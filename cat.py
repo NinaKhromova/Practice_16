8ebe8dd20293b76c5cde5875c8b2a2d7f188395a
unknown <falling2@yandex.ru>
2021-12-30 17:41:11 +0300

    123

--- cat.py
+++ cat.py
@@ -1,17 +1,11 @@
 class Cat:
-    def _init_(self,name,gender,age,registration_document,status):
+    def _init_(self,name,gender,age):
      self.name = name
      self.gender = gender
      self.age = age
-     self.registration_document=registration_document
-     self.status=status
     def getName(self):
         return self.name
     def getGender(self):
         return self.gender
     def getAge(self):
         return self.age
-    def getRegistration_document(self):
-        return self.registration_document
-    def getStatus(self):
-        return self.status
