713d772e91e1691b62b986396b944eb0fe41c4e6
unknown <falling2@yandex.ru>
2021-12-30 18:16:43 +0300

    123

---
+++ Cat_test.py
@@ -0,0 +1,6 @@
+from cat import Cat
+cat_1 = Cat('Sam', 'boy', 2)
+cat_2 = Cat('Baron', 'boy', 2)
+print('Первый кот:', cat_1.name, cat_1.gender, cat_1.age)
+print('Второй кот:', cat_2.name, cat_2.gender, cat_2.age)
+
--- cat.py
+++ cat.py
@@ -1,11 +1,14 @@
 class Cat:
-    def _init_(self,name,gender,age):
-     self.name = name
-     self.gender = gender
-     self.age = age
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
     def getName(self):
         return self.name
+
     def getGender(self):
         return self.gender
+
     def getAge(self):
         return self.age
